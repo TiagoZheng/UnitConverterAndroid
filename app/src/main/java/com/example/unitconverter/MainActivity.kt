e4712c6d88330cfc9586236ffb728d7fc354705e
tiagozheng <tiago.zheng23@gmail.com>
2025-05-08 15:39:09 +0100

    Unit converter app basics - Text, textfield

---
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.unitconverter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.unitconverter.ui.theme.UnitConverterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            UnitConverterTheme {
+                UnitConverter()
+            }
+        }
+    }
+}
+
+
+@Composable
+fun UnitConverter() {
+    Column {
+        // Here all the UI elements will be stacked below each other
+        Text("Unit Converter")
+        OutlinedTextField(value = "", onValueChange = {
+            // Here goes what should happen, when the Value of our OutlinedTextField changes
+        })
+
+        Row {
+            // Here all the UI elements will be stacked next to each other
+        }
+        Text("Hello There")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UnitConverterPreview() {
+    UnitConverter()
+}
