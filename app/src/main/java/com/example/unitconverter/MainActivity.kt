c40cd1f6de7eba266c6bcbec13a4cc0043c5b447
tiagozheng <tiago.zheng23@gmail.com>
2025-05-08 16:08:19 +0100

    Buttons Text and icons

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -1,22 +1,19 @@
 package com.example.unitconverter
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.unitconverter.ui.theme.UnitConverterTheme
 
@@ -42,13 +39,21 @@ fun UnitConverter() {
         })
 
         Row {
-            val context = LocalContext.current
-            // Here all the UI elements will be stacked next to each other
-            Button(onClick = { Toast.makeText(context, "Thanks for clicking!", Toast.LENGTH_LONG).show() })
-            {
-                Text("Click Me!")
+            Box{
+                Button(onClick = { }) {
+                    Text("Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "")
+                }
+            }
+
+            Box{
+                Button(onClick = { }) {
+                    Text("Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "")
+                }
             }
         }
+
         Text("Result: ")
     }
 }
