ea7a0f6a7e6d28f3438fbc2290639312b7cdd9a0
tiagozheng <tiago.zheng23@gmail.com>
2025-05-08 16:46:58 +0100

    DropDown Menu, Spacer

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -8,10 +8,16 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -19,6 +25,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,15 +44,20 @@ class MainActivity : ComponentActivity() {
 fun UnitConverter() {
     Column(
         modifier =  Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment =  Alignment.CenterHorizontally
+        verticalArrangement = (Arrangement.Center),
+        horizontalAlignment =  Alignment.CenterHorizontally,
     ) {
         // Here all the UI elements will be stacked below each other
 
-        Text("Unit Converter")
+        Text("Unit Converter",
+            //modifier = Modifier.padding(16.dp)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(value = "", onValueChange = {
             // Here goes what should happen, when the Value of our OutlinedTextField changes
         })
+
+        Spacer(modifier = Modifier.height(16.dp))
 
         Row {
             Box{
@@ -53,15 +65,54 @@ fun UnitConverter() {
                     Text("Select")
                     Icon(Icons.Default.ArrowDropDown, contentDescription = "")
                 }
+                DropdownMenu(expanded = true, onDismissRequest = {}) {
+                    DropdownMenuItem(
+                        text = { Text("Centimeter")},
+                        onClick = { })
+
+                    DropdownMenuItem(
+                        text = { Text("Meters")},
+                        onClick = { })
+
+                    DropdownMenuItem(
+                        text = { Text("Feet")},
+                        onClick = { })
+
+                    DropdownMenuItem(
+                        text = { Text("Millimeters")},
+                        onClick = { })
+                }
             }
+
+            Spacer(modifier = Modifier.width(16.dp))
 
             Box{
                 Button(onClick = { }) {
                     Text("Select")
                     Icon(Icons.Default.ArrowDropDown, contentDescription = "")
                 }
+
+                DropdownMenu(expanded = true, onDismissRequest = {}) {
+                    DropdownMenuItem(
+                        text = { Text("Centimeter")},
+                        onClick = { })
+
+                    DropdownMenuItem(
+                        text = { Text("Meters")},
+                        onClick = { })
+
+                    DropdownMenuItem(
+                        text = { Text("Feet")},
+                        onClick = { })
+
+                    DropdownMenuItem(
+                        text = { Text("Millimeters")},
+                        onClick = { })
+                }
             }
         }
+
+        Spacer(modifier = Modifier.height(16.dp))
 
         Text("Result: ")
     }
